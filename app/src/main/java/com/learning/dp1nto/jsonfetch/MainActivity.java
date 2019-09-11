aa82a9218fab44f32c0e4364f1374d23ec6cab0b
Daniel <dpintobenitez@gmail.com>
2019-09-11 22:26:16 +0200

    Working json fetch

--- app/src/main/java/com/learning/dp1nto/jsonfetch/MainActivity.java
+++ app/src/main/java/com/learning/dp1nto/jsonfetch/MainActivity.java
@@ -2,12 +2,32 @@ package com.learning.dp1nto.jsonfetch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button click;
+    public static TextView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        click = (Button) findViewById(R.id.fetchButton);
+        data = (TextView) findViewById(R.id.fetchData);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FetchData process = new FetchData();
+                process.execute();
+
+            }
+        });
+
     }
 }
